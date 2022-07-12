66e6c67f3ff90fd51bac38de636ba789cd7a15ad
LTP <1069113473@qq.com>
2022-07-12 08:34:23 +0800

    完成基础codeLab

--- Compose/src/main/java/com/btpj/compose/MainActivity.kt
+++ Compose/src/main/java/com/btpj/compose/MainActivity.kt
@@ -3,14 +3,21 @@ package com.btpj.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.btpj.compose.ui.theme.AndroidPracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,27 +25,69 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidPracticeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MessageCard(msg = Message(R.drawable.test, "开始学习Compose", "2022-04-05"))
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!", color = Color.Red)
-}
+    data class Message(val picRes: Int, val content: String, val date: String)
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AndroidPracticeTheme {
-        Greeting("Android")
+    @Composable
+    fun MessageCard(msg: Message) {
+        Row(
+            modifier = Modifier
+                .padding(all = 20.dp)
+                .fillMaxWidth()
+        ) {
+            Image(
+                painter = painterResource(id = msg.picRes),
+                contentDescription = "图片内容描述",
+                modifier = Modifier
+                    .size(40.dp)
+                    .clip(CircleShape)
+                    .border(width = 1.dp, color = MaterialTheme.colors.primary)
+            )
+
+            Spacer(modifier = Modifier.width(8.dp))
+
+            var isExpanded by remember { mutableStateOf(false) }
+
+            Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+                Text(
+                    text = msg.content,
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1
+                )
+                Spacer(
+                    modifier = Modifier
+                        .height(4.dp)
+                )
+                Text(text = msg.date)
+            }
+        }
+    }
+
+    @Composable
+    fun MessageList(msgList: List<Message>) {
+        LazyColumn {
+            items(msgList) { msg -> MessageCard(msg = msg) }
+        }
+    }
+
+    @Preview(showSystemUi = true)
+    @Composable
+    fun PreviewMessageCard() {
+        val msgList = ArrayList<Message>()
+        for (i in 0..100) {
+            msgList.add(
+                Message(
+                    R.drawable.test,
+                    "开始学习Compose第${i}天开始学习Compose第${i}天开始学习Compose第${i}天开始学习Compose第${i}天开始学习Compose第${i}天",
+                    "2022-04-05"
+                )
+            )
+        }
+        AndroidPracticeTheme {
+            MessageList(msgList = msgList)
+        }
     }
 }
---
+++ Compose/src/main/java/com/btpj/compose/ui/basic/BasicActivity.kt
@@ -0,0 +1,103 @@
+package com.btpj.compose.ui.basic
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExitToApp
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.btpj.compose.ui.theme.AndroidPracticeTheme
+
+class BasicActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AndroidPracticeTheme {
+                App()
+            }
+        }
+    }
+}
+
+@Composable
+fun App() {
+    var showList by rememberSaveable { mutableStateOf(false) }
+
+    if (showList) {
+        ListScreen()
+    } else {
+        WelcomeScreen(onContinueClick = { showList = true })
+    }
+}
+
+@Composable
+fun WelcomeScreen(onContinueClick: () -> Unit) {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.primary
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("欢迎来到Compose")
+            Button(onClick = onContinueClick, modifier = Modifier.padding(10.dp)) {
+                Text("进入列表")
+            }
+        }
+
+    }
+}
+
+@Composable
+fun ListScreen(strList: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = strList) { item -> Item(item) }
+    }
+}
+
+@Composable
+fun Item(item: String) {
+    var expanded by rememberSaveable { mutableStateOf(false) }
+
+    Card(
+        backgroundColor = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 10.dp)
+    ) {
+        Row {
+            Column(modifier = Modifier.weight(1f)) {
+                Text("Hello")
+                Text(text = item)
+                if (expanded) {
+                    Text(text = ("我是隐藏文本").repeat(10))
+                }
+            }
+            IconButton(onClick = { expanded = !expanded }) {
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = ""
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun DefaultPreview() {
+    AndroidPracticeTheme {
+        App()
+    }
+}
